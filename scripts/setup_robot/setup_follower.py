aaa71e334936c92a955ce7ae6547e4668531cadc
Le Minh Triet <trietlm0306@gmail.com>
2026-04-13 22:49:41 +0700

    feat: add GUI and example scripts

--- scripts/setup_robot/setup_follower.py
+++
@@ -1,9 +0,0 @@
-from lerobot.robots.so_follower import SO101Follower, SO101FollowerConfig
-
-config = SO101FollowerConfig(
-    port="COM6",
-    id="DI_VLA_FOLLOWER",
-)
-follower = SO101Follower(config)
-follower.setup_motors()
-
